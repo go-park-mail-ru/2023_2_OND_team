726d78b2e4726315c299ad66202bcd891e0964ae
Gvidow <arzamachev.art-alex@yandex.ru>
2023-10-02 03:17:07 +0300

    TP-933 update: handlers pin and format response

--- internal/pkg/service/pin.go
+++ internal/pkg/service/pin.go
@@ -2,11 +2,7 @@ package service
 
 import (
 	"errors"
-	"fmt"
 	"net/http"
-
-	"net/url"
-	"strconv"
 
 	_ "github.com/go-park-mail-ru/2023_2_OND_team/internal/pkg/entity/pin"
 
@@ -30,60 +26,20 @@ func (s *Service) GetPins(w http.ResponseWriter, r *http.Request) {
 	s.log.Info("request on get pins", log.F{"method", r.Method}, log.F{"path", r.URL.Path})
 	SetContentTypeJSON(w)
 
-	count, lastID, err := fetchValidParamForLoadTape(r.URL)
+	count, lastID, err := FetchValidParamForLoadTape(r.URL)
 	if err != nil {
-		s.log.Info("parse body error", log.F{"error", err.Error()})
-		fmt.Fprintln(w, "{\"status\": \"error\"}")
-		return
-	}
-
-	s.log.Sugar().Infof("param: count=%d, lastID=%d", count, lastID)
-
-	pins, last := s.pinCase.SelectNewPins(r.Context(), count, lastID)
-	fmt.Fprintf(w, `{"status": "ok",
-	"message": "download new pins",
-	"body": {
-		"pins": %v,
-		"lastID": %d
-	}
-	}`, pins, last)
-}
-
-func fetchValidParamForLoadTape(u *url.URL) (count int, lastID int, err error) {
-	if param := u.Query().Get("count"); len(param) > 0 {
-		c, err := strconv.ParseInt(param, 10, 64)
-		if err != nil {
-			return 0, 0, fmt.Errorf("fetch count param for load tape: %w", err)
-		}
-		count = int(c)
+		s.log.Info("parse url query params", log.F{"error", err.Error()})
+		err = responseError(w, "bad_params",
+			"expected parameters: count(positive integer: [1; 1000]), lastID(positive integer, the absence of this parameter is equal to the value 0)")
 	} else {
-		return 0, 0, ErrCountParameterMissing
+		s.log.Sugar().Infof("param: count=%d, lastID=%d", count, lastID)
+		pins, last := s.pinCase.SelectNewPins(r.Context(), count, lastID)
+		err = responseOk(w, "pins received are sorted by id", map[string]any{
+			"pins":   pins,
+			"lastID": last,
+		})
 	}
-	if param := u.Query().Get("lastID"); len(param) > 0 {
-		last, err := strconv.ParseInt(param, 10, 64)
-		if err != nil {
-			return 0, 0, fmt.Errorf("fetch lastID param for load tape: %w", err)
-		}
-		lastID = int(last)
+	if err != nil {
+		s.log.Error(err.Error())
 	}
-	if count <= 0 || lastID < 0 {
-		return 0, 0, ErrBadParams
-	}
-	return
-}
-
-// GetPinByID godoc
-//
-//	@Description	Get concrete pin by id
-//	@Tags			Pin
-//	@Produce		json
-//	@Param			pinId	path		int	true	"Id of the pin"
-//	@Success		200		{object}	JsonResponse{body=Pin}
-//	@Failure		400		{object}	JsonErrResponse
-//	@Failure		404		{object}	JsonErrResponse
-//	@Failure		500		{object}	JsonErrResponse
-//	@Router			/api/v1/pin/{pinId} [get]
-func (s *Service) GetPinByID(w http.ResponseWriter, r *http.Request) {
-	s.log.Info("it worked GetPinByID")
-	fmt.Fprintf(w, "{\"status\": \"ok\", \"path\": \"%s\", \"method\": \"%s\"}\n", r.URL.Path, r.Method)
 }
--- internal/pkg/service/response.go
+++ internal/pkg/service/response.go
@@ -1,6 +1,8 @@
 package service
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
 )
 
@@ -15,9 +17,38 @@ type JsonResponse struct {
 type JsonErrResponse struct {
 	Status  string `json:"status" example:"error"`
 	Message string `json:"message" example:"Error description"`
-	Code    int    `json:"code,string"`
+	Code    string `json:"code"`
 } // @name JsonErrResponse
 
 func SetContentTypeJSON(w http.ResponseWriter) {
 	w.Header().Set("Content-Type", "application/json")
+}
+
+func responseOk(w http.ResponseWriter, message string, body any) error {
+	res := JsonResponse{
+		Status:  "ok",
+		Message: message,
+		Body:    body,
+	}
+	resBytes, err := json.Marshal(res)
+	if err != nil {
+		return fmt.Errorf("responseOk: %w", err)
+	}
+	w.WriteHeader(http.StatusOK)
+	_, err = w.Write(resBytes)
+	return err
+}
+
+func responseError(w http.ResponseWriter, code, message string) error {
+	res := JsonErrResponse{
+		Status:  "error",
+		Message: message,
+		Code:    code,
+	}
+	resBytes, err := json.Marshal(res)
+	if err != nil {
+		return fmt.Errorf("responseError: %w", err)
+	}
+	_, err = w.Write(resBytes)
+	return err
 }
