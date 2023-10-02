6b6e1a356f3f40f0ab8431b6a7b045617614b76a
wonderf00l <maks22036@gmail.com>
2023-10-03 01:54:13 +0300

    TP-565 update: finished TestGetPins

--- internal/pkg/service/pin_test.go
+++ internal/pkg/service/pin_test.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"io"
 	"net/http/httptest"
-	"reflect"
 	"testing"
 
 	"github.com/go-park-mail-ru/2023_2_OND_team/internal/pkg/repository/ramrepo"
@@ -39,8 +38,7 @@ func TestGetPins(t *testing.T) {
 				Body: map[string]interface{}{
 					"lastID": 3,
 					"pins": []interface{}{
-						map[string]interface{}{"id": 2, "picture": "/2"},
-						map[string]interface{}{"id": 3, "picture": "/3"},
+						map[string]interface{}{"id": 3},
 					},
 				},
 			},
@@ -53,8 +51,8 @@ func TestGetPins(t *testing.T) {
 				Body: map[string]interface{}{
 					"lastID": 5,
 					"pins": []interface{}{
-						map[string]interface{}{"id": 4, "picture": "/4"},
-						map[string]interface{}{"id": 5, "picture": "/5"},
+						map[string]interface{}{"id": 4},
+						map[string]interface{}{"id": 5},
 					},
 				},
 			},
@@ -71,81 +69,94 @@ func TestGetPins(t *testing.T) {
 			body, _ := io.ReadAll(resp.Body)
 			var actualResp JsonResponse
 
-			json.Unmarshal(body, &actualResp)
+			json.Unmarshal(body, &actualResp) // после Unmarshall числа приводятся к float64
 			fmt.Println(tCase.expResp)
 			fmt.Println(actualResp)
-			require.True(t, reflect.DeepEqual(tCase.expResp, actualResp))
-			// require.EqualExportedValues(t, tCase.expResp, actualResp)
+			require.Equal(t, tCase.expResp.Status, actualResp.Status)
+			require.Equal(t, tCase.expResp.Message, actualResp.Message)
+			// fmt.Println(reflect.TypeOf(actualResp.Body.(map[string]interface{})["lastID"]))
+			expLastID := tCase.expResp.Body.(map[string]interface{})["lastID"].(int)
+			actualLastID := actualResp.Body.(map[string]interface{})["lastID"].(float64)
+
+			expIDs, actualIDs := make([]int, 0), make([]int, 0)
+			for _, pin := range tCase.expResp.Body.(map[string]interface{})["pins"].([]interface{}) {
+				expIDs = append(expIDs, pin.(map[string]interface{})["id"].(int))
+			}
+			for _, pin := range actualResp.Body.(map[string]interface{})["pins"].([]interface{}) {
+				actualIDs = append(actualIDs, int(pin.(map[string]interface{})["id"].(float64)))
+			}
+
+			require.Equal(t, expLastID, int(actualLastID))
+			require.Equal(t, expIDs, actualIDs)
 		})
 	}
 
-	// badCases := []struct {
-	// 	rawURL  string
-	// 	expResp JsonErrResponse
-	// }{
-	// 	{
-	// 		rawURL: fmt.Sprintf("%s?count=%d&lastID=%d", rawUrl, 0, 3),
-	// 		expResp: JsonErrResponse{
-	// 			Status:  "error",
-	// 			Message: "expected parameters: count(positive integer: [1; 1000]), lastID(positive integer, the absence of this parameter is equal to the value 0)",
-	// 			Code:    "bad_params",
-	// 		},
-	// 	},
-	// 	{
-	// 		rawURL: fmt.Sprintf("%s?count=%d&lastID=%d", rawUrl, -2, 3),
-	// 		expResp: JsonErrResponse{
-	// 			Status:  "error",
-	// 			Message: "expected parameters: count(positive integer: [1; 1000]), lastID(positive integer, the absence of this parameter is equal to the value 0)",
-	// 			Code:    "bad_params",
-	// 		},
-	// 	},
-	// 	{
-	// 		rawURL: fmt.Sprintf("%s?count=%d&lastID=%d", rawUrl, 213123, 3),
-	// 		expResp: JsonErrResponse{
-	// 			Status:  "error",
-	// 			Message: "expected parameters: count(positive integer: [1; 1000]), lastID(positive integer, the absence of this parameter is equal to the value 0)",
-	// 			Code:    "bad_params",
-	// 		},
-	// 	},
-	// 	{
-	// 		rawURL: fmt.Sprintf("%s?count=%d&lastID=%d", rawUrl, 0, -1),
-	// 		expResp: JsonErrResponse{
-	// 			Status:  "error",
-	// 			Message: "expected parameters: count(positive integer: [1; 1000]), lastID(positive integer, the absence of this parameter is equal to the value 0)",
-	// 			Code:    "bad_params",
-	// 		},
-	// 	},
-	// 	{
-	// 		rawURL: fmt.Sprintf("%s?count=&lastID=%d", rawUrl, 3),
-	// 		expResp: JsonErrResponse{
-	// 			Status:  "error",
-	// 			Message: "expected parameters: count(positive integer: [1; 1000]), lastID(positive integer, the absence of this parameter is equal to the value 0)",
-	// 			Code:    "bad_params",
-	// 		},
-	// 	},
-	// 	{
-	// 		rawURL: fmt.Sprintf("%s?lastID=%d", rawUrl, 3),
-	// 		expResp: JsonErrResponse{
-	// 			Status:  "error",
-	// 			Message: "expected parameters: count(positive integer: [1; 1000]), lastID(positive integer, the absence of this parameter is equal to the value 0)",
-	// 			Code:    "bad_params",
-	// 		},
-	// 	},
-	// }
+	badCases := []struct {
+		rawURL  string
+		expResp JsonErrResponse
+	}{
+		{
+			rawURL: fmt.Sprintf("%s?count=%d&lastID=%d", rawUrl, 0, 3),
+			expResp: JsonErrResponse{
+				Status:  "error",
+				Message: "expected parameters: count(positive integer: [1; 1000]), lastID(positive integer, the absence of this parameter is equal to the value 0)",
+				Code:    "bad_params",
+			},
+		},
+		{
+			rawURL: fmt.Sprintf("%s?count=%d&lastID=%d", rawUrl, -2, 3),
+			expResp: JsonErrResponse{
+				Status:  "error",
+				Message: "expected parameters: count(positive integer: [1; 1000]), lastID(positive integer, the absence of this parameter is equal to the value 0)",
+				Code:    "bad_params",
+			},
+		},
+		{
+			rawURL: fmt.Sprintf("%s?count=%d&lastID=%d", rawUrl, 213123, 3),
+			expResp: JsonErrResponse{
+				Status:  "error",
+				Message: "expected parameters: count(positive integer: [1; 1000]), lastID(positive integer, the absence of this parameter is equal to the value 0)",
+				Code:    "bad_params",
+			},
+		},
+		{
+			rawURL: fmt.Sprintf("%s?count=%d&lastID=%d", rawUrl, 0, -1),
+			expResp: JsonErrResponse{
+				Status:  "error",
+				Message: "expected parameters: count(positive integer: [1; 1000]), lastID(positive integer, the absence of this parameter is equal to the value 0)",
+				Code:    "bad_params",
+			},
+		},
+		{
+			rawURL: fmt.Sprintf("%s?count=&lastID=%d", rawUrl, 3),
+			expResp: JsonErrResponse{
+				Status:  "error",
+				Message: "expected parameters: count(positive integer: [1; 1000]), lastID(positive integer, the absence of this parameter is equal to the value 0)",
+				Code:    "bad_params",
+			},
+		},
+		{
+			rawURL: fmt.Sprintf("%s?lastID=%d", rawUrl, 3),
+			expResp: JsonErrResponse{
+				Status:  "error",
+				Message: "expected parameters: count(positive integer: [1; 1000]), lastID(positive integer, the absence of this parameter is equal to the value 0)",
+				Code:    "bad_params",
+			},
+		},
+	}
 
-	// for _, tCase := range badCases {
-	// 	t.Run("TestGetPins bad", func(t *testing.T) {
-	// 		req := httptest.NewRequest("GET", tCase.rawURL, nil)
-	// 		w := httptest.NewRecorder()
-	// 		service.GetPins(w, req)
+	for _, tCase := range badCases {
+		t.Run("TestGetPins bad", func(t *testing.T) {
+			req := httptest.NewRequest("GET", tCase.rawURL, nil)
+			w := httptest.NewRecorder()
+			service.GetPins(w, req)
 
-	// 		resp := w.Result()
-	// 		body, _ := io.ReadAll(resp.Body)
-	// 		var actualResp JsonResponse
+			resp := w.Result()
+			body, _ := io.ReadAll(resp.Body)
+			var actualResp JsonErrResponse
 
-	// 		json.Unmarshal(body, actualResp)
-	// 		require.Equal(t, tCase.expResp, actualResp)
-	// 	})
-	// }
-
+			json.Unmarshal(body, &actualResp)
+			require.Equal(t, tCase.expResp, actualResp)
+		})
+	}
 }
