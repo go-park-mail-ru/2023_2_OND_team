c228379d89bc27f83dcbf624add1d9284934c85a
Gvidow <arzamachev.art-alex@yandex.ru>
2023-10-04 13:13:51 +0300

    TP-a49 add: log fileserver

--- cmd/fileserver/main.go
+++ cmd/fileserver/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"net/http"
 )
 
@@ -11,5 +12,7 @@ func main() {
 		Addr:    ":8081",
 		Handler: http.StripPrefix("/upload/", fs),
 	}
-	s.ListenAndServe()
+	log.Println("fileserver start")
+	defer log.Panicln("fileserver finish")
+	s.ListenAndServeTLS("/home/ond_team/cert/fullchain.pem", "/home/ond_team/cert/privkey.pem")
 }
