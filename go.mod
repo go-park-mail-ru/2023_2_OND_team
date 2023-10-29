aa62ad329cceb9ae23067021f0e18db0dc531367
Gvidow <arzamachev.art-alex@yandex.ru>
2023-10-29 16:31:53 +0300

    TP-2f3 add: repository with storage in redis

--- go.mod
+++ go.mod
@@ -9,6 +9,7 @@ require (
 	github.com/google/uuid v1.3.1
 	github.com/jackc/pgx/v5 v5.4.3
 	github.com/proullon/ramsql v0.0.1
+	github.com/redis/go-redis/v9 v9.2.1
 	github.com/rs/cors v1.10.1
 	github.com/stretchr/testify v1.8.4
 	github.com/swaggo/http-swagger v1.3.4
@@ -20,7 +21,9 @@ require (
 
 require (
 	github.com/KyleBanks/depth v1.2.1 // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/go-openapi/jsonpointer v0.20.0 // indirect
 	github.com/go-openapi/jsonreference v0.20.2 // indirect
 	github.com/go-openapi/spec v0.20.9 // indirect
--- internal/app/app.go
+++ internal/app/app.go
@@ -2,14 +2,16 @@ package app
 
 import (
 	"context"
+	"time"
 
 	"github.com/jackc/pgx/v5/pgxpool"
+	redis "github.com/redis/go-redis/v9"
 
 	"github.com/go-park-mail-ru/2023_2_OND_team/internal/api/server"
 	"github.com/go-park-mail-ru/2023_2_OND_team/internal/api/server/router"
 	deliveryHTTP "github.com/go-park-mail-ru/2023_2_OND_team/internal/pkg/delivery/http/v1"
 	pinRepo "github.com/go-park-mail-ru/2023_2_OND_team/internal/pkg/repository/pin"
-	"github.com/go-park-mail-ru/2023_2_OND_team/internal/pkg/repository/ramrepo"
+	sessionRepo "github.com/go-park-mail-ru/2023_2_OND_team/internal/pkg/repository/session"
 	userRepo "github.com/go-park-mail-ru/2023_2_OND_team/internal/pkg/repository/user"
 	"github.com/go-park-mail-ru/2023_2_OND_team/internal/pkg/usecase/pin"
 	"github.com/go-park-mail-ru/2023_2_OND_team/internal/pkg/usecase/session"
@@ -18,26 +20,34 @@ import (
 )
 
 func Run(ctx context.Context, log *log.Logger, configFile string) {
-	pool, err := pgxpool.New(ctx, "postgres://ond_team:love@localhost:5432/pinspire?search_path=pinspire")
+	ctxApp, cancel := context.WithTimeout(ctx, 10*time.Second)
+	defer cancel()
+
+	pool, err := pgxpool.New(ctxApp, "postgres://ond_team:love@localhost:5432/pinspire?search_path=pinspire")
 	if err != nil {
 		log.Error(err.Error())
 		return
 	}
 	defer pool.Close()
 
-	err = pool.Ping(ctx)
+	err = pool.Ping(ctxApp)
 	if err != nil {
 		log.Error(err.Error())
 		return
 	}
 
-	db, err := ramrepo.OpenDB("RamRepository")
-	if err != nil {
+	redisCl := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "love",
+	})
+
+	status := redisCl.Ping(ctxApp)
+	if status.Err() != nil {
 		log.Error(err.Error())
 		return
 	}
 
-	sm := session.New(log, ramrepo.NewRamSessionRepo(db))
+	sm := session.New(log, sessionRepo.NewSessionRepo(redisCl))
 	userCase := user.New(log, userRepo.NewUserRepoPG(pool))
 	pinCase := pin.New(log, pinRepo.NewPinRepoPG(pool))
 
