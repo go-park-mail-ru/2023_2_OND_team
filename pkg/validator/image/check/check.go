76c0e217b11a6281a6bcc9498e658552a0f3aa0d
Gvidow <arzamachev.art-alex@yandex.ru>
2023-11-06 02:32:47 +0300

    TP-erros update: both sides fall into range

--- pkg/validator/image/check/check.go
+++ pkg/validator/image/check/check.go
@@ -5,7 +5,7 @@ type CheckSize func(width, height float64) bool
 func BothSidesFallIntoRange(minSize, maxSize int) CheckSize {
 	return func(width, height float64) bool {
 		if width < float64(minSize) || width > float64(maxSize) ||
-			height < float64(minSize) || height > float64(minSize) {
+			height < float64(minSize) || height > float64(maxSize) {
 
 			return false
 		}
