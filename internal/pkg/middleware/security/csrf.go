e51042a69909cb72fa4acf6b21be954eeeffe47a
Gvidow <arzamachev.art-alex@yandex.ru>
2023-11-08 12:52:12 +0300

    dev2 del: status code 204 on path /api/v1/csrf

--- internal/pkg/middleware/security/csrf.go
+++ internal/pkg/middleware/security/csrf.go
@@ -13,7 +13,6 @@ func CSRF(cfg CSRFConfig) mw.Middleware {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			if r.URL.Path == cfg.PathToGet && r.Method == http.MethodGet {
-				w.WriteHeader(http.StatusNoContent)
 				setToken(w, &cfg)
 				return
 			}
