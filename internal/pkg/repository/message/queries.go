7155b873349c6bb479206368773e3ebac0acfe1d
Gvidow <arzamachev.art-alex@yandex.ru>
2023-11-19 18:58:37 +0300

    TP-f07 update sql queries

--- internal/pkg/repository/message/queries.go
+++ internal/pkg/repository/message/queries.go
@@ -1,10 +1,10 @@
 package message
 
 const (
-	SelectMessageByID     = "SELECT user_from, user_to, content WHERE id = $1 AND deleted_at IS NULL;"
+	SelectMessageByID     = "SELECT user_from, user_to, content FROM message WHERE id = $1 AND deleted_at IS NULL;"
 	SelectMessageFromChat = `SELECT id, user_from, user_to, content
 							 FROM message 
-							 WHERE deleted_at IS NULL AND id < $1 AND
+							 WHERE deleted_at IS NULL AND (id < $1 OR $1 = 0) AND
 							 	(user_from = $2 AND user_to = $3 OR user_from = $3 AND user_to = $2)
 							 ORDER BY id DESC
 							 LIMIT $4;`
