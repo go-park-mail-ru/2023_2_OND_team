5f210acdde0a60d5859a8883ed2a4ad2cee99b8c
wonderf00l <maks22036@gmail.com>
2023-11-21 03:12:23 +0300

    TP-500 update: add global errors package with general application error types, errors

--- internal/pkg/repository/errors.go
+++ internal/pkg/repository/errors.go
@@ -1,11 +1,10 @@
 package repository
 
-import "errors"
-
-const (
-	TimeFormat = "02.01.2006"
+import (
+	"errors"
 )
 
+// for backward compatibility
 var (
 	ErrMethodUnimplemented = errors.New("unimplemented")
 	ErrNoData              = errors.New("got no data from repository layer")
