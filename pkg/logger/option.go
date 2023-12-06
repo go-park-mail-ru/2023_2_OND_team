e38aacff5591fb0cf49a27bab98964f3619e69d0
Gvidow <arzamachev.art-alex@yandex.ru>
2023-12-06 12:18:03 +0300

    TP-3a4 add: output files flag for logging

--- pkg/logger/option.go
+++ pkg/logger/option.go
@@ -18,3 +18,15 @@ func SetFormatTime(layout string) ConfigOption {
 func RFC3339FormatTime() ConfigOption {
 	return SetFormatTime(time.RFC3339)
 }
+
+func SetOutputPaths(files ...string) ConfigOption {
+	return func(cfg *zap.Config) {
+		cfg.OutputPaths = files
+	}
+}
+
+func SetErrorOutputPaths(files ...string) ConfigOption {
+	return func(cfg *zap.Config) {
+		cfg.ErrorOutputPaths = files
+	}
+}
